dfacd115aa2c0e7fd0de4f6cb37b96d5a3dcb3fc
queequeg-johnson <mouse@DESKTOP-BJQOIRL.localdomain>
2026-01-29 18:44:12 -0800

    Added some C code

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cjson/cJSON.h>
+#include <math.h>
+#include <curl/curl.h>
+
+#define API_KEY "b07024da23edbf67525c2bbe2786aafb"
+#define CITY "Los Angeles"
+#define COUNTRY "US"
+
+int main(void){
+	CURL *curl;
+	CURLcode res;
+	char url[512];
+
+
+}
